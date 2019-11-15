a3f662d26b3e16a4ba79b1bf6f83fcc85dc212de
kantivekariya <hiren.patel@medsymphony.com>
2019-11-15 17:21:52 +0530

    update port

--- comman/config/env.config.js
+++ comman/config/env.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "SERVER_PORT": 3600,
     "JWT_KEY": "myS33!!creeeT",
-    "JWT_EXPIRATION_IN_SECONDS":36000,
+    "JWT_EXPIRATION_IN_SECONDS": 36000,
     "environment": "dev",
     "DB_CONNECTION": "mongodb://localhost:27017/manage_user"
 };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const config = require('./comman/config/env.config');
 const http = require('http');
 const app = require('./app');
 
-const port = config.SERVER_PORT || 3000;
+const port = config.SERVER_PORT;
 const server = http.createServer(app);
 
 server.listen(port);
